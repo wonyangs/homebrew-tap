198673a497f42f8e155a2710b7684c20fb5fad9f
wonyangs <bennyws99@gmail.com>
2026-09-01 10:43:28 +0900

    poke-pack-bar 0.4.2

--- Casks/poke-pack-bar.rb
+++ Casks/poke-pack-bar.rb
@@ -1,6 +1,6 @@
 cask "poke-pack-bar" do
-  version "0.4.1"
-  sha256 "5dabe0cd5a4e220d2719006212b08db9d840ec990d04340bf34e7dea9fc1c920"
+  version "0.4.2"
+  sha256 "7cebebfee4b37c9b62dca5d05a900b0ed64a98d7b43b9ede6bac7c3608b52c89"
 
   url "https://github.com/wonyangs/PokePackBar/releases/download/v#{version}/PokePackBar.zip"
   name "PokePackBar"
